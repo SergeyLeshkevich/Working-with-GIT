73a118c2fa04ad13f8c41a9e4769c29380583b85
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2023-10-09 21:49:34 +0300

    feat: add function 3 (develop)

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("feat: add function 1 (develop)");
         System.out.println("feat: add function 2 (develop)");
+        System.out.println("feat: add function 3 (develop)");
     }
 }
