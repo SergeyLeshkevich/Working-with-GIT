33b152fbc29b1cfec7fc1fdf22327bae652db2a0
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2023-10-09 21:56:13 +0300

    feat: add function 2 (feature/for_next_release)

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("feat: add function 2 (develop)");
         System.out.println("feat: add function 3 (develop)");
         System.out.println("feat: add function 1 (feature/for_next_release)");
+        System.out.println("feat: add function 2 (feature/for_next_release)");
     }
 }
