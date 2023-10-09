717e2fab3b6284346785bd079415a7dce0e3bec8
Sergey Leshkevich <s.leshkevichemail@gmail.com>
2023-10-09 21:59:42 +0300

    feat: add method2 (for_future_release)

--- FutureReleaseClass.java
+++ FutureReleaseClass.java
@@ -2,4 +2,7 @@ public class FutureReleaseClass {
     public void method1() {
         System.out.println("method1");
     }
+    public void method2() {
+        System.out.println("method2");
+    }
 }
